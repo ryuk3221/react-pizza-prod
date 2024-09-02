8b59ea507a70af4660f932c4a6c2b57182ee124c
Danil <danilmitrofanov123@gmail.com>
2024-09-02 17:05:00 +0300

    update

--- src/components/NotFoundBlock/index.jsx
+++ src/components/NotFoundBlock/index.jsx
@@ -1,11 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import logo from "../../assets/img/pizza-logo.svg";
 import styles from "./NotFoundBlock.module.scss";
 
 const NotFoundBlock = () => {
   return (
     <div>
       <h1 className={styles.title}>Ничего не найдено :(</h1>
-      <button className={styles.button}>Вернуться назад</button>
+        <Link to="/">
+          <div className="header__logo">
+            <img width="38" src={logo} alt="Pizza logo" />
+            <div>
+              <h1>React Pizza</h1>
+              <p>самая вкусная пицца во вселенной</p>
+            </div>
+          </div>
+        </Link>
     </div>
   );
 };
